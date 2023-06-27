3e005c143bad29c4e104cf39f24dc656e1af7dea
amme987 <emmagination97@gmail.com>
2023-06-27 11:23:08 -0400

    Show text informing that passwords do not match

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const button = document.querySelector("button");
 const error = document.querySelectorAll(".error");
 const password = document.querySelectorAll("input.error");
+const selectMismatch = document.querySelector(".select-mismatch");
 
 button.addEventListener("click", mismatch);
 
@@ -8,9 +9,14 @@ function mismatch() {
   error.forEach(item => console.log(item.value));
   if (error[0].value !== error[1].value) {
     password.forEach(item => (item.style.outline = "1px solid red"));
+    const mismatch = document.createElement("div");
+    mismatch.textContent = "*Passwords do not match";
+    mismatch.style.fontSize = "small";
+    mismatch.style.color = "red";
+    selectMismatch.appendChild(mismatch);
   } else {
     password.forEach(item => (item.style.outline = "none"));
   }
 }
 
-//TODO: Space form elements, Show password mismatch error
+//TODO: Space form elements
